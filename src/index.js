a358e9e185ef71ca2e384860a9bf6d84f8afa738
JMGino <jmgino909@gmail.com>
2023-09-16 14:48:35 -0600

    working...

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+  {
+    author: 'Mark R. Levin',
+    title: 'The Democrate Party Hates America',
+    img: 'https://m.media-amazon.com/images/I/61sp+sMfrXL._SL1500_.jpg',
+    id: 1
+  },
+  {
+    author: 'Walter Isaacson',
+    title: 'Elon Musk',
+    img: 'https://m.media-amazon.com/images/I/814mI0-rkxL._SL1500_.jpg',
+    id: 2
+  },
+  {
+    author: 'Arthur C. Brooks',
+    title: 'Build the Life You Want: The Art and Science of Getting Happier',
+    img: 'https://m.media-amazon.com/images/I/81+MJor-K6L._SL1500_.jpg',
+    id: 3
+  }
+];
+
+const BookList = () => {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        const { id } = book
+        return (
+          <Book {...book} key={id}/>
+        )
+      })}
+    </section>
+  )
+}
+
+const Book = (props) => {
+  const { author, title, img } = props
+  return (
+    <article className='book'>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+const root =ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList />)
